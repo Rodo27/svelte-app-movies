8ac32ec8c1ab2fc228b5f42d0a7c1297a484c398
Aldo Rodrigo Hernandez <aldo.hernandez@cmgo.org.mx>
2022-07-13 10:22:36 -0500

    working with reactivity and transitions

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,62 @@
 <script>  
 
-  import {onMount} from 'svelte'
+  //import {onMount} from 'svelte'
+  //import { debug } from 'svelte/internal'
+  import {fly} from 'svelte/transition'
   import MovieItem from './Movie/Item.svelte'
 
   const APIKEY = '29ed1d64cc3508c30f08131eb1860d99'
   const BASEURL = 'https://api.themoviedb.org/3'
   const APISETTINGS = `?api_key=${APIKEY}&language=es-MX`
 
-  let movies = []
+  /* let movies = []
 
   function fetchMovies (){
     const URL = `${BASEURL}/discover/movie${APISETTINGS}&sort_by=popularity.desc`
 
-    fetch(URL).then(res => res.json()).then(({results}) =>{movies =  results})
+    fetch(URL).then(res => res.json()).then(({results}) =>{movies =  results,  console.log(movies)})
+
+  } */
+
+  const movies =  (async () => {
+
+    const URL = `${BASEURL}/discover/movie${APISETTINGS}&sort_by=popularity.desc`
+    const response = await fetch(URL)
+
+    return await response.json()
+  })()
+
+  let likedMovies = []
+
+  function toggleLike(event){
+    //console.log(event)
+
+    const movie = event.detail
+
+    let index = likedMovies.findIndex(m=> m.id === movie.id)
+
+    if(index >= 0){
+      likedMovies.splice(index,1)
+      console.log(likedMovies)
+    }else{
+      likedMovies.push(movie)
+      console.log(likedMovies)
+    }
+
+    likedMovies = likedMovies
 
   }
 
-  onMount(()=>{
+  $: like =  (id) => {
+    let index = likedMovies.findIndex(m => m.id === id)
+
+    return index >= 0
+  }
+
+  /* onMount(()=>{
     console.log('Component has been mounted!')
     fetchMovies()
-  })
+  }) */
 
 </script>
 
@@ -28,13 +65,42 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </svelte:head>
 
-<main class="container">
+<main class="container-fluid">
   <div class="row">
-    {#each movies as movie}
-      <div class="col-12 col-md-6 col-lg-3 p-1">
-        <MovieItem></MovieItem>
+    <div class="col-12 col-md-6 col-lg-8 border panel">
+      <h2>Popularity Movies</h2>
+
+      <div class="row">
+        {#await movies}
+        <!-- promise is pending-->
+          <div class="col-12"><p>loading...</p></div>
+        {:then data}
+          <!-- {@debug data} -->
+          <!-- promise was fulfilled-->
+          {#each data.results as movie}
+            <div class="col-12 col-md-6 col-lg-4 p-1">
+              <MovieItem like={like(movie.id)} id={movie.id} title={movie.title} overview={movie.overview} cover={movie.poster_path} on:onToggleLike={toggleLike}></MovieItem>
+            </div>
+          {/each}
+        {/await}
       </div>
-    {/each}
+      
+    </div>
+    <div class="col-12 col-md-6 col-lg-4 border panel">
+      <h2>Favorite Movies</h2>
+      <div class="row">
+        {#if likedMovies.length}
+          {#each likedMovies as movie, i (movie.id)}
+            <div in:fly="{{duration:200, y:20}}"  out:fly="{{duration:800, y:-20}}" class="col-12 col-md-6 col-lg-4 p-1">
+              <MovieItem like={like(movie.id)} id={movie.id} title={movie.title} overview="" cover={movie.cover} on:onToggleLike={toggleLike}></MovieItem>
+            </div>
+          {/each}
+        {:else}
+            <div class="col-12"><p>No favorite movies</p></div>
+        {/if}
+        
+      </div>
+    </div>
   </div>
 </main>
 
@@ -42,6 +108,11 @@
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  .panel {
+    height: 100vh;
+    overflow: auto;
   }
 
   main {
--- src/Movie/Item.svelte
+++ src/Movie/Item.svelte
@@ -1,20 +1,30 @@
 <script>
 
+    import {createEventDispatcher} from 'svelte'
+
     export let id
     export let title
     export let overview
+    export let cover
+    export let like
+
+    $: coverURL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2'+cover
+
+    const dispatch = createEventDispatcher()
+
+    function toggleLike(){
+        dispatch('onToggleLike', {id, title, overview, cover})
+    }
+
 </script>
 
 <div class="card">
-    <img src="..." class="card-img-top" alt="..." />
+    <img src={coverURL} class="card-img-top" alt="..." />
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-        </p>
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{overview}</p>
     </div>
-    <div class="text-center">
-        <button class="btn btn-primary">Like</button>
+    <div class="text-center mb-2">
+        <button class={like ? 'btn btn-secondary' : 'btn btn-primary'} on:click={toggleLike}>{like ? 'Unlike': 'Like'}</button>
     </div>
 </div>
